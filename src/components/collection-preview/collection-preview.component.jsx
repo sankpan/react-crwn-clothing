2d417007ee6f84a97fea6318673bcf2d713c3342
Sanjeev Panicker/USA <Sanjeevkumar.Panicker@cushwake.com>
2020-02-18 20:48:47 -0500

    Added shop page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
@@ -7,8 +8,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className='preview'>
       {items
         .filter((item, indx) => indx < 4)
-        .map(item => (
-          <div key={item.id}>{item.name}</div>
+        .map(({ id, ...otherItemFlds }) => (
+          <CollectionItem id={id} {...otherItemFlds} />
         ))}
     </div>
   </div>
